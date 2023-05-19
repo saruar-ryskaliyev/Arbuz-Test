6c8c531c51ef8ca2c6db0dc8a3d5d35392368f42
saruar-ryskaliyev <108226977+saruar-ryskaliyev@users.noreply.github.com>
2023-05-19 19:48:17 +0600

    Add files via upload

--- ArbuzTest/ViewController.swift
+++ ArbuzTest/ViewController.swift
@@ -10,10 +10,12 @@ import UIKit
 
 
 
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
-
-
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UISearchBarDelegate, UISearchResultsUpdating {
+ 
+   
     
+    private var searching = false
+    private var searchedFood = [PhotoCollectionViewCellModel]()
     private var products = [Product]()
     private var viewModels = [PhotoCollectionViewCellModel]()
     private var collectionView: UICollectionView?
@@ -21,8 +23,117 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "arbuz.kz 🍉"
         
 
+        setupCollectionView()
+        setupNavigationBar()
+        setupSearchController()
+
+        
+        fetchProducts()
+        
+    }
+    
+    
+    
+    //MARK: - Search Controller
+    
+    func setupSearchController(){
+        let searchController = UISearchController()
+        navigationItem.searchController = searchController
+        
+        searchController.searchBar.delegate = self
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.enablesReturnKeyAutomatically = false
+        searchController.searchBar.enablesReturnKeyAutomatically = false
+        searchController.searchBar.returnKeyType = UIReturnKeyType.done
+        self.navigationItem.searchController = searchController
+        searchController.searchBar.placeholder = "Искать на Арбузе"
+        
+    }
+    
+    
+    
+    //MARK: - NavigationBar
+    
+    func setupNavigationBar(){
+        let appearance = UINavigationBarAppearance()
+        appearance.titleTextAttributes = [.foregroundColor: UIColor(red: 0.10, green: 0.67, blue: 0.29, alpha: 1.00)]
+        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor(red: 0.10, green: 0.67, blue: 0.29, alpha: 1.00)]
+
+        navigationItem.standardAppearance = appearance
+        navigationItem.scrollEdgeAppearance = appearance
+    }
+    
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        let searchText = searchController.searchBar.text!
+        if !searchText.isEmpty{
+            searching = true
+            searchedFood.removeAll()
+            for viewModel in viewModels{
+                if viewModel.label.lowercased().contains(searchText.lowercased()){
+                    searchedFood.append(viewModel)
+                }
+            }
+            
+        }else{
+            searching = false
+            searchedFood.removeAll()
+            searchedFood = viewModels
+        }
+        
+        DispatchQueue.main.async {
+            self.collectionView?.reloadData()
+        }
+    }
+    
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searching = false
+        searchedFood.removeAll()
+        DispatchQueue.main.async {
+            self.collectionView?.reloadData()
+        }
+    }
+
+    
+    //MARK: - Collection View
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    
+        if searching == false{
+            return viewModels.count
+        }else{
+            return searchedFood.count
+        }
+ 
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        if searching == false{
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoCollectionViewCell.identifier, for: indexPath) as! PhotoCollectionViewCell
+            
+            cell.configure(with: viewModels[indexPath.row])
+
+            return cell
+        } else{
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoCollectionViewCell.identifier, for: indexPath) as! PhotoCollectionViewCell
+            
+            cell.configure(with: searchedFood[indexPath.row])
+
+            return cell
+        }
+        
+
+    }
+    
+    func setupCollectionView(){
+        
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .vertical
         layout.minimumLineSpacing = 1
@@ -36,33 +147,18 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
             return
         }
         
-
+    
         collectionView.delegate = self
         collectionView.dataSource = self
         collectionView.register(PhotoCollectionViewCell.self, forCellWithReuseIdentifier: PhotoCollectionViewCell.identifier)
         view.addSubview(collectionView)
         
         collectionView.frame = view.bounds
-        
-        fetchProducts()
-        
-    }
-
-
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return viewModels.count
     }
     
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoCollectionViewCell.identifier, for: indexPath) as! PhotoCollectionViewCell
-        
-        cell.configure(with: viewModels[indexPath.row])
-
-        return cell
-    }
     
-    
+    //MARK: - API Fetching
     
     private func fetchProducts(){
         APICaller.shared.parse { [weak self] result in
